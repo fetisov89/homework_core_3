d743e222e6d6bba03b10918bceb9fddb5d50dd43
Илья <fetisov.ilya.andreevich@gmail.com>
2022-07-14 21:21:50 +0300

    Finished

---
+++ src/ServiceTransport.java
@@ -0,0 +1,17 @@
+public interface ServiceTransport {
+    void updateTyre(Truck truck);
+
+    void updateTyre(Car car);
+
+    void updateTyre(Bicycle bicycle);
+
+    void checkEngine(Truck truck);
+
+    void checkEngine(Car car);
+
+    void checkTrailer(Truck truck);
+
+
+}
+
+
